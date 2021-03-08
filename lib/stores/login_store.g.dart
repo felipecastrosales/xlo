8b37211f8618536f6aece7838a089fa301b7f5f9
Felipe Sales <[soufeliposales@gmail.com]>
2021-03-08 19:37:04 -0300

    Validating Login Fields

---
+++ lib/stores/login_store.g.dart
@@ -0,0 +1,121 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$LoginStore on _LoginStore, Store {
+  Computed<bool> _$emailValidComputed;
+
+  @override
+  bool get emailValid =>
+      (_$emailValidComputed ??= Computed<bool>(() => super.emailValid,
+              name: '_LoginStore.emailValid'))
+          .value;
+  Computed<bool> _$passwordValidComputed;
+
+  @override
+  bool get passwordValid =>
+      (_$passwordValidComputed ??= Computed<bool>(() => super.passwordValid,
+              name: '_LoginStore.passwordValid'))
+          .value;
+  Computed<Function> _$loginPressedComputed;
+
+  @override
+  Function get loginPressed =>
+      (_$loginPressedComputed ??= Computed<Function>(() => super.loginPressed,
+              name: '_LoginStore.loginPressed'))
+          .value;
+
+  final _$emailAtom = Atom(name: '_LoginStore.email');
+
+  @override
+  String get email {
+    _$emailAtom.reportRead();
+    return super.email;
+  }
+
+  @override
+  set email(String value) {
+    _$emailAtom.reportWrite(value, super.email, () {
+      super.email = value;
+    });
+  }
+
+  final _$passwordAtom = Atom(name: '_LoginStore.password');
+
+  @override
+  String get password {
+    _$passwordAtom.reportRead();
+    return super.password;
+  }
+
+  @override
+  set password(String value) {
+    _$passwordAtom.reportWrite(value, super.password, () {
+      super.password = value;
+    });
+  }
+
+  final _$loadingAtom = Atom(name: '_LoginStore.loading');
+
+  @override
+  bool get loading {
+    _$loadingAtom.reportRead();
+    return super.loading;
+  }
+
+  @override
+  set loading(bool value) {
+    _$loadingAtom.reportWrite(value, super.loading, () {
+      super.loading = value;
+    });
+  }
+
+  final _$_loginAsyncAction = AsyncAction('_LoginStore._login');
+
+  @override
+  Future<void> _login() {
+    return _$_loginAsyncAction.run(() => super._login());
+  }
+
+  final _$_LoginStoreActionController = ActionController(name: '_LoginStore');
+
+  @override
+  void setEmail(String value) {
+    final _$actionInfo =
+        _$_LoginStoreActionController.startAction(name: '_LoginStore.setEmail');
+    try {
+      return super.setEmail(value);
+    } finally {
+      _$_LoginStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void setPassword(String value) {
+    final _$actionInfo = _$_LoginStoreActionController.startAction(
+        name: '_LoginStore.setPassword');
+    try {
+      return super.setPassword(value);
+    } finally {
+      _$_LoginStoreActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+email: ${email},
+password: ${password},
+loading: ${loading},
+emailValid: ${emailValid},
+passwordValid: ${passwordValid},
+loginPressed: ${loginPressed}
+    ''';
+  }
+}
