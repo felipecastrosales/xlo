e0527b538da9b242339cff50163daa4943ff7cf3
Felipe Sales <[soufeliposales@gmail.com]>
2021-05-19 09:53:05 -0300

    Validating Fields - Part #1

--- lib/screens/create/components/images_field.dart
+++ lib/screens/create/components/images_field.dart
@@ -14,71 +14,97 @@ class ImagesField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     void onImageSelected(File image) {
       createStore.images.add(image);
       Navigator.of(context).pop();
     };
-    return Container(
-      color: Colors.grey[200],
-      height: 120,
-      child: Observer(
-        builder: (_) {
-          return ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: createStore.images.length < 3 
-                ? createStore.images.length + 1 : 5,
-            itemBuilder: (_, index) {
-              if (index == createStore.images.length) {
-                return Padding(
-                  padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
-                  child: GestureDetector(
-                    onTap: () {
-                      if (Platform.isAndroid) {
-                        showModalBottomSheet(
-                          context: context,
-                          builder: (_) => ImageSourceModel(onImageSelected),
-                        );
-                      } else {
-                        showCupertinoModalPopup(
-                          context: context,
-                          builder: (_) => ImageSourceModel(onImageSelected),
-                        );
-                      }
-                    },
-                    child: CircleAvatar(
-                      radius: 44,
-                      backgroundColor: Colors.grey[300],
-                      child: Icon(
-                        Icons.camera_alt, size: 40, color: Colors.grey,
-                      ),
-                    ),
-                  ),
-                );
-              } else {
-                return Padding(
-                  padding:  EdgeInsets.fromLTRB(
-                      8, 16, index == 4 ? 8 : 0, 16),
-                  child: GestureDetector(
-                    onTap: (){
-                      showDialog(
-                        context: context, 
-                        builder: (_) => ImageDialog(
-                          image: createStore.images[index],
-                          onDelete: () => createStore.images.removeAt(index),
+
+    return Column(
+      children: [
+        Container(
+          color: Colors.grey[200],
+          height: 120,
+          child: Observer(
+            builder: (_) {
+              return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: createStore.images.length < 3 
+                    ? createStore.images.length + 1 : 5,
+                itemBuilder: (_, index) {
+                  if (index == createStore.images.length) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(8, 8, 0, 8),
+                      child: GestureDetector(
+                        onTap: () {
+                          if (Platform.isAndroid) {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (_) => ImageSourceModel(onImageSelected),
+                            );
+                          } else {
+                            showCupertinoModalPopup(
+                              context: context,
+                              builder: (_) => ImageSourceModel(onImageSelected),
+                            );
+                          }
+                        },
+                        child: CircleAvatar(
+                          radius: 44,
+                          backgroundColor: Colors.grey[300],
+                          child: Icon(
+                            Icons.camera_alt, size: 40, color: Colors.grey,
+                          ),
                         ),
-                      );
-                    },
-                    child: CircleAvatar(
-                      radius: 44, 
-                      backgroundImage: FileImage(createStore.images[index]),
-                    ),
-                  ),
-                ); 
-              }
+                      ),
+                    );
+                  } else {
+                    return Padding(
+                      padding:  EdgeInsets.fromLTRB(
+                          8, 16, index == 4 ? 8 : 0, 16),
+                      child: GestureDetector(
+                        onTap: (){
+                          showDialog(
+                            context: context, 
+                            builder: (_) => ImageDialog(
+                              image: createStore.images[index],
+                              onDelete: () =>
+                                  createStore.images.removeAt(index),
+                            ),
+                          );
+                        },
+                        child: CircleAvatar(
+                          radius: 44, 
+                          backgroundImage: FileImage(createStore.images[index]),
+                        ),
+                      ),
+                    ); 
+                  }
+                },
+              );
             },
-          );
-        },
-      ),
+          ),
+        ),
+        Observer(
+          builder: (_) {
+            if (createStore.imagesError != null) {
+              return Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.fromLTRB(16, 8, 0, 8),
+                decoration: BoxDecoration(
+                  border: Border(top: BorderSide(color: Colors.red)),
+                ),
+                child: Text(
+                  createStore.imagesError, 
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+              );
+            } else { 
+              return Container(); 
+            }
+          },
+        ),
+      ],
     );
   }
 }
--- lib/stores/create_store.dart
+++ lib/stores/create_store.dart
@@ -9,15 +9,61 @@ class CreateStore = _CreateStoreBase with _$CreateStore;
 abstract class _CreateStoreBase with Store {
   ObservableList images = ObservableList();
 
+  @computed
+  bool get imagesValid => images.isNotEmpty;
+  String get imagesError {
+    if (imagesValid) {
+      return null;
+    } else {
+      return 'Insira imagens';
+    }
+  }
+
+  @observable
+  String title = '';
+
+  @action
+  void setTitle(String value) => title = value;
+
+  @computed
+  bool get titleValid => title.length >= 6;
+  String get titleError {
+    if (titleValid) {
+      return null;
+    } else if (title.isEmpty) {
+      return 'Campo obrigatório';
+    } else {
+      return 'Título muito curto';
+    }
+  }
+
+  @observable
+  String description = '';
+
+  @action 
+  void setDescription(String value) => description = value;
+
+    @computed
+  bool get descriptionValid => description.length >= 10;
+  String get descriptionError {
+    if (descriptionValid) {
+      return null;
+    } else if (description.isEmpty) {
+      return 'Campo obrigatório';
+    } else {
+      return 'Descrição muito curta';
+    }
+  }
+
   @observable
   Category category;
 
-  @action 
+  @action
   void setCategory(Category value) => category = value;
 
   @observable
   bool hidePhone = false;
 
-  @action 
+  @action
   void setHidePhone(bool value) => hidePhone = value;
 }
