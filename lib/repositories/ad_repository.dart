d11d6ce954823244b67cfc7d4aff76fb816d4a79
Felipe Sales <[soufeliposales@gmail.com]>
2021-05-23 07:50:03 -0300

    Saving images in Parse

--- lib/repositories/ad_repository.dart
+++ lib/repositories/ad_repository.dart
@@ -1,5 +1,39 @@
+import 'dart:io';
+
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:path/path.dart' as path;
+
 import '../models/ad.dart';
+import 'parse_errors.dart';
 
 class AdRepository {
-  void save(Ad ad) {}
+  Future<void> save(Ad ad) async {
+    final parseImages = await saveImages(ad.images);
+  }
+
+  Future<List<ParseFile>> saveImages(List images) async {
+    final parseImages = <ParseFile>[];
+    try {
+      for (final image in images) {
+        if (image is File) {
+          final parseFile = ParseFile(image, name: path.basename(image.path));
+          final response = await parseFile.save();
+          if (!response.success) {
+            return Future.error(
+              ParseErrors.getDescription(response.error.code),
+            );
+          }
+          parseImages.add(parseFile);
+        } else {
+          final parseFile = ParseFile(null);
+          parseFile.name = path.basename(image);
+          parseFile.url = image;
+          parseImages.add(parseFile);
+        }
+      }
+      return parseImages;
+    } catch (e) {
+      return Future.error('Falha ao salvar imagem');
+    }
+  }
 }
