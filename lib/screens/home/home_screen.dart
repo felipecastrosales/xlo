b9f26cccbf4f71dba0af3d7a12ccbdb992a124bb
Felipe Sales <[soufeliposales@gmail.com]>
2021-05-26 06:57:13 -0300

    Creating the SearchField

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 
 import '../../components/custom_drawer/custom_drawer.dart';
+import 'components/search_dialog.dart';
 
 class HomeScreen extends StatelessWidget {
+  openSearch(BuildContext context) async {
+    final search = await showDialog(
+      context: context, builder: (_) => SearchDialog(currentSearch: 'Opa!'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         drawer: CustomDrawer(),
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                openSearch(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
